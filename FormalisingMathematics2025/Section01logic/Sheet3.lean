2b5f1a0d07199ffb984cdc6b5dfb7ba0341d2957
Tyler Perkins <tyler@clortox.com>
2025-10-08 19:57:25 -0400

    Save progress today

--- FormalisingMathematics2025/Section01logic/Sheet3.lean
+++ FormalisingMathematics2025/Section01logic/Sheet3.lean
@@ -34,45 +34,130 @@ variable (P Q R : Prop)
 
 example : ¬True → False := by
   intro h
+
   exact h True.intro
+
   done
 
 example : False → ¬True := by
-  sorry
+  exact False.elim
   done
 
 example : ¬False → True := by
-  sorry
+  intro h
+  exact True.intro
+
   done
 
 example : True → ¬False := by
-  sorry
+  intro h
+  exact False.elim
   done
 
 example : False → ¬P := by
-  sorry
+  intro false
+
+  exfalso
+
+  exact false
+
   done
 
 example : P → ¬P → False := by
-  sorry
+
+  intro p notp
+
+  apply notp
+
+  exact p
+
   done
 
 example : P → ¬¬P := by
-  sorry
+  intro p
+
+  by_contra hfalse
+
+  apply hfalse
+
+  exact p
+
   done
 
 example : (P → Q) → ¬Q → ¬P := by
-  sorry
+  intro p_imp_q notq
+
+  by_cases h : P
+
+  · -- case p
+    exact fun _hp =>
+      notq (p_imp_q h)
+  · -- case ¬ p
+    exact h
+
   done
 
 example : ¬¬False → False := by
-  sorry
+  intro not_not_false
+
+  by_cases h : False
+
+  exact h
+
+  apply not_not_false at h
+
+  apply h
+
   done
 
 example : ¬¬P → P := by
-  sorry
+
+  intro not_not_p
+
+  by_cases h : P
+
+  -- P
+
+  exact h
+
+  -- P
+
+  apply not_not_p at h
+
+  exfalso
+
+  exact h
+
   done
 
 example : (¬Q → ¬P) → P → Q := by
-  sorry
+  intro notq_notp p
+
+
+
+  apply notq_notp at p
+
+  exfalso
+  exact p
+
+
+
+
+
+
+
+
+
+
+
+  -- by_cases h : Q
+  -- exact h
+  -- apply notq_notp at h
+
+
+
+
+
+
+
   done
